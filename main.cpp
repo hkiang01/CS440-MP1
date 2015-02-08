dfcc724aa471917ffd77d4153aebc1dbfa1d5f46
harry <harry@crunchbang>
2015-02-07 19:41:26 -0600

    initial parsing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,46 @@
 #include <fstream>
 #include <iostream>
+using namespace std;
+
+struct cell{
+
+	bool wall;
+};
+
+
+void process_line(cell** maze, string curr_line, int curr_height)
+{
+
+	maze[curr_height] = new cell[curr_line.length()];
+	int curr_col = 0;
+	for(string::iterator it = curr_line.begin(); it != curr_line.end(); it++)
+	{
+		char test = *it;
+		if(test == '%')
+		{
+			//wall
+			maze[curr_height][curr_col].wall = true;
+
+		}
+		else if(test == 'P')
+		{
+			//starting point
+			maze[curr_height][curr_col].wall = false;
+		}
+		else if(test == '.')
+		{
+			//goal
+			maze[curr_height][curr_col].wall = false;
+		}
+		else
+		{
+			//open space 
+			maze[curr_height][curr_col].wall = false;
+		}
+		curr_col++;
+	}
+
+}
 
 
 int main(void)
@@ -9,43 +50,32 @@ int main(void)
 	int small_cols = 0;
 	int small_rows = 0;
 
-	std::ifstream myfile ("smallMaze.txt");
+	ifstream myfile ("smallMaze.txt");
 	//std::ofstream outfile ("output_small.txt");
 
 	if(myfile.is_open())
 	{
-		std::string curr_line;
+		string curr_line;
+
+
+		if(getline(myfile, curr_line) == null) {
+			cout << "Error: no lines read " << endl;
+			return;
+		}
+		
+		small_cols = curr_line.length();
+		cell** maze = new cell*[small_cols];
+		process_line(maze, curr_line, small_rows);
+		small_rows++;
+
 		while(getline(myfile, curr_line))
 		{
-			small_cols = curr_line.size();
-			small_rows++;
-
-
-			for(std::string::iterator it = curr_line.begin(); it != curr_line.end(); it++)
-			{
-				char test = *it;
-				if(test == '%')
-				{
-					//wall
-
-				}
-				else if(test == 'P')
-				{
-					//starting point
-				}
-				else if(test == '.')
-				{
-					//goal
-				}
-				else
-				{
-					//open space 
-				}
-			}
+			process_line(maze, curr_line, small_rows);
+			small_rows;
 		}
 	}
 	else
 	{
-		std::cout << "Unable to open file." << std::endl;
+		cout << "Unable to open file." << std::endl;
 	}
 }
